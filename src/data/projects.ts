8818e3a8e5832ea4009d99ad8c41e4a6eb391488
Katherine Aguilera <katherine.aguilera@wundermanthompson.com>
2026-01-16 12:42:27 -0500

    (feat): Portfolio 2026

---
+++ src/data/projects.ts
@@ -0,0 +1,91 @@
+export const projects = [
+  {
+    id: 1,
+    types: "confidential",
+    title: "APP Jelpit Conjuntos",
+    desc: "Worked as a Frontend Developer on the Jelpit Conjuntos app, utilizing technologies such as Ionic, React, Sass, and the BEM methodology.",
+    img: "projects/appConjuntos.webp",
+    tags: ["Ionic", "React", "Sass", "BEM"],
+  },
+  {
+    id: 2,
+    title: "APP Bolívar Conmigo",
+    desc: "Contributed as a Frontend Developer to the \"Bolívar Conmigo\" project, leveraging Ionic, Angular, Sass, and BEM methodology.",
+    img: "projects/appBolivar.webp",
+    tags: ["Angular", "CSS3" , "Ionic", "BEM"],
+    github: "",
+    demo: "https://apps.apple.com/co/app/bol%C3%ADvar-conmigo/id1551406375"
+  },
+  {
+    id: 3,
+    title: "Web Jelpit Conjuntos",
+    desc: "Frontend Developer for the \"Jelpit Conjuntos\" project, employing React, Sass, and BEM methodology.",
+    img: "projects/conjuntosDesktop.webp",
+    tags: ["React", "TypeScript", "AWS"],
+    github: "",
+    demo: "https://www.jelpit.com/"
+  },
+    {
+    id: 4,
+    title: "Landing Page TR",
+    desc: "Landing page built with Sass and Vanilla JS.",
+    img: "projects/webTruora.webp",
+    tags: ["CSS3", "HTML5", "Responsive"],
+    github: "https://github.com/KatherinAguilera/LandingTR",
+    demo: "https://onboarding-landing.netlify.app/"
+  },
+  {
+    id: 5,
+    title: "Poke API",
+    desc: "High-performance PokéDex featuring RESTful API integration, dynamic filtering, and responsive design",
+    img: "projects/pokeApi.webp",
+    tags: ["React", "CSS3", "Responsive"],
+    github: "https://github.com/KatherinAguilera/api-poke-ReactHooks/tree/main",
+    demo: "https://pokemon-api-hooks.netlify.app/"
+  },
+    {
+    id: 6,
+    title: "Forecast Poster",
+    desc: "Interactive web platform for generating custom posters based on user-defined parameters.",
+    img: "projects/webForecast.webp",
+    tags: ["JavaScript", "Sass", "HTML5", "Canvas"],
+    github: "",
+    demo: "https://forecast-k.netlify.app/"
+  },
+  {
+    id: 7,
+    title: "Dynamic web World Cup",
+    desc: "Interactive 2022 World Cup product catalog featuring a region-based redirection system to official local retailers.",
+    img: "projects/wwc2022.webp",
+    tags: ["JavaScript", "Sass", "HTML5", "Canvas"],
+    github: "",
+    demo: "https://wwc-multifeds.netlify.app/"
+  },
+    {
+    id: 8,
+    title: "Landing with chatbot",
+    desc: "Responsive web interface integrated with a dynamic chatbot engine, featuring custom interaction flows and adaptive UI.",
+    img: "projects/ibm.webp",
+    tags: ["JavaScript", "Responsive", "IA"],
+    github: "",
+    demo: "https://bright-cendol-66c9ec.netlify.app"
+  },
+  {
+    id: 9,
+    title: "Geek Characters",
+    desc: "AI-Powered 'Geek Persona' Generator: A web app that profiles users via a questionnaire and synthesizes custom character avatars using AI facial analysis and transformation.",
+    img: "projects/geek.webp",
+    tags: ["FaceAPI", "Responsive", "IA"],
+    github: "",
+    demo: "https://harmonious-kashata-1f9222.netlify.app/"
+  },
+  {
+    id: 10,
+    title: "Preferer center",
+    desc: "Dynamic landing page integrated with Microsoft Dynamics 365, designed to segment users based on their preferences and trigger personalized automated email workflows.",
+    img: "projects/apc.webp",
+    tags: ["MSD", "Email Marketing", "IA"],
+    github: "",
+    demo: "https://whimsical-rolypoly-c46427.netlify.app/"
+  },
+];
