390772f17bbd14c62fd9c8a95bfb6361ba67f417
Katherine Aguilera <katherine.aguilera@wundermanthompson.com>
2026-01-16 16:14:24 -0500

    (feat): deploy3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  // La URL de tu sitio en GitHub Pages
   site: 'https://KatherinAguilera.github.io',
-  // El nombre exacto de tu repositorio
-  base: '/katherineaguilera',
+  base: process.env.NODE_ENV === 'production' ? '/katherineaguilera' : '/',
 });
